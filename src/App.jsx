8a1a54e01738c2efe9a844485e5a561fafa84d0e
ziadmo5amed <161350029+ziadmo5amed@users.noreply.github.com>
2025-09-28 21:58:18 +0300

    First commit - add project

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Nav from './components/navigation/Nav';
+ 
+ 
 import {Route , Routes , Link} from "react-router-dom";
+import Header from './components/header/Header';
 function App() {
  
   return (
     <>
     <Nav />
-   
+    <Header />
  
-    </>
+
+ </>
   )
 }
 
